4b22de451818a37fea953da277f85a3677e496a5
Alexis Rolland <arolland@univ-tours.local>
2021-12-21 17:22:23 +0100

    add basic behavior functions : at, join and msg

--- lib_LoRaE5.c
+++ lib_LoRaE5.c
@@ -14,6 +14,9 @@
 
 /* Directives de compilation		*/
 
+const uint8_t   E5_AT_RESP[]="+AT: OK";
+const uint8_t   E5_JOIN_DONE_RESP[]="+JOIN: Done";
+const uint8_t   E5_MSG_DONE_RESP[]="+MSG: Done";
 
 /* Déclarations des variables globales 	*/
 uint8_t CarRec;
@@ -22,13 +25,93 @@ uint8_t TxBuffer[BUFFER_SIZE];
 uint8_t RxPtr=0;
 RxStatus_t  RxStatus = WAIT_SOL;
 
+// Time out variables
+uint8_t TargetTimeOut;
+uint8_t CurrNbTimoutTics;
+TimeOutStatus_t  TimeOutStatus;
+
+uint8_t RxFrame[BUFFER_SIZE];
+
 
 /*	Implémentation du code */
 //------------------------------------------------------------------------------
-e5_error_t  E5_test_AT(void)
+e5_error_t  E5_CheckConnection(void)
 {
+    if (TimeOutStatus != TIME_OUT_STOPPED) return E5_ERROR;
     sprintf(TxBuffer,"AT\n");
+    
     E5_ll_send(TxBuffer);
+    E5_startTimeOut(1);
+    
+    while( (TimeOutStatus != TIME_OUT_EXPIRED) && (RxStatus != EOL_RECEIVED));
+    
+    if (TimeOutStatus == TIME_OUT_EXPIRED){
+        E5_ClearTimeOutStatus();    // Stop time out timer & clear time out status (no longer needed)
+        return E5_TIME_OUT;
+        }
+        
+    E5_ClearTimeOutStatus();    // Stop time out timer & clear time out status
+    E5_trim(RxFrame,RxBuffer);
+    RxStatus = WAIT_SOL;
+    
+    if (strcmp(RxFrame,E5_AT_RESP) != 0) return E5_ERROR;
+    
+    return E5_OK;
+}
+//------------------------------------------------------------------------------
+e5_error_t  E5_Join(bool_t ForceJoin)
+{
+    if (TimeOutStatus != TIME_OUT_STOPPED) return E5_ERROR;
+    
+    if (ForceJoin == false) sprintf(TxBuffer,"AT+JOIN\n");
+    else sprintf(TxBuffer,"AT+JOIN=FORCE\n");
+    
+    E5_ll_send(TxBuffer);
+    E5_startTimeOut(20);
+    
+    do{
+        while( (TimeOutStatus != TIME_OUT_EXPIRED) && (RxStatus != EOL_RECEIVED));
+    
+        if (TimeOutStatus == TIME_OUT_EXPIRED){
+            E5_ClearTimeOutStatus();    // Stop time out timer & clear time out status (no longer needed)
+            return E5_TIME_OUT;
+            }
+        E5_ReStartTimeOut();
+        
+        E5_trim(RxFrame,RxBuffer);
+        RxStatus = WAIT_SOL;
+        
+    } while (strcmp(RxFrame,E5_JOIN_DONE_RESP) != 0);                                   
+    E5_ClearTimeOutStatus();    // Stop time out timer & clear time out status
+    
+    return E5_OK;
+}
+//------------------------------------------------------------------------------
+e5_error_t  E5_SendStrMsg(uint8_t *pMsg)
+{
+    if (TimeOutStatus != TIME_OUT_STOPPED) return E5_ERROR;
+    
+    sprintf(TxBuffer,"AT+MSG=\"%s\"\n",pMsg);
+    
+    E5_ll_send(TxBuffer);
+    E5_startTimeOut(20);
+    
+    do{
+        while( (TimeOutStatus != TIME_OUT_EXPIRED) && (RxStatus != EOL_RECEIVED));
+    
+        if (TimeOutStatus == TIME_OUT_EXPIRED){
+            E5_ClearTimeOutStatus();    // Stop time out timer & clear time out status (no longer needed)
+            return E5_TIME_OUT;
+            }
+        E5_ReStartTimeOut();
+        
+        E5_trim(RxFrame,RxBuffer);
+        RxStatus = WAIT_SOL;
+        
+    } while (strcmp(RxFrame,E5_MSG_DONE_RESP) != 0);                                   
+    E5_ClearTimeOutStatus();    // Stop time out timer & clear time out status
+    
+    
     return E5_OK;
 }
 
@@ -42,6 +125,7 @@ e5_error_t  E5_sendVER(void)
 
 //------------------------------------------------------------------------------
 
+//------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 void    E5_ll_send(uint8_t *pstr)
@@ -54,7 +138,48 @@ void    E5_ll_send(uint8_t *pstr)
     }
 }
 //------------------------------------------------------------------------------
-
+void    E5_trim(uint8_t *pDest, uint8_t *pSource)
+{
+    while (*pSource != '\0')
+    {
+        if ((*pSource != 0x0A) && (*pSource != 0x0D))
+        {
+            *pDest = *pSource;
+            pDest++;
+        }
+        pSource++;
+    }
+    //pDest++;
+    *pDest = '\0';
+}
+//------------------------------------------------------------------------------
+void    E5_startTimeOut(uint8_t Nb500ms)
+{
+    if (Nb500ms < 1) return;
+    
+    TargetTimeOut = Nb500ms;
+    CurrNbTimoutTics = 0;  
+    TimeOutStatus = TIME_OUT_RUNNING;
+    IEC0bits.T1IE = 1; // Enable T1 IRQ
+    T1CONbits.TON = 1; // Start Timer
+}
+//------------------------------------------------------------------------------
+TimeOutStatus_t E5_getTimeOutStatus(void)
+{
+    return TimeOutStatus;
+}
+//------------------------------------------------------------------------------
+void    E5_ClearTimeOutStatus(void)
+{
+    T1CONbits.TON = 0;  // Stop Timer
+    IEC0bits.T1IE = 0;  // Disable T1 IRQ
+    TimeOutStatus = TIME_OUT_STOPPED;
+}
+//------------------------------------------------------------------------------
+void    E5_ReStartTimeOut(void)
+{
+    if (TimeOutStatus == TIME_OUT_RUNNING) CurrNbTimoutTics = 0;
+}
 //------------------------------------------------------------------------------
 void _ISR __attribute__((no_auto_psv))  _U2RXInterrupt(void)
 {   
@@ -63,6 +188,7 @@ void _ISR __attribute__((no_auto_psv))  _U2RXInterrupt(void)
     {
         case WAIT_SOL: 
             if (CarRec == E5_SOL){
+                CurrNbTimoutTics=0;     // Extends time out (just in case...)
                 RxPtr=0;
                 RxBuffer[RxPtr] = CarRec;
                 RxStatus = RX_ON;
@@ -85,4 +211,17 @@ void _ISR __attribute__((no_auto_psv))  _U2RXInterrupt(void)
     
 	IFS1bits.U2RXIF = 0;    // RxFlag
 }
-
+//------------------------------------------------------------------------------
+void _ISR __attribute__((no_auto_psv))  _T1Interrupt(void)
+{
+    LATAbits.LATA0 = ~LATAbits.LATA0;   // Debug purposes
+    CurrNbTimoutTics++;
+    if (CurrNbTimoutTics >= TargetTimeOut)
+    {
+        TimeOutStatus = TIME_OUT_EXPIRED;
+        T1CONbits.TON = 0;  // Stop Timer
+        IEC0bits.T1IE = 0;  // Disable T1 IRQ
+    }
+    IFS0bits.T1IF = 0;
+}
+//------------------------------------------------------------------------------
--- lib_LoRaE5.h
+++ lib_LoRaE5.h
@@ -14,16 +14,36 @@
 
 #include <xc.h>
 #include <stdio.h>
-
+#include <string.h>
+typedef enum {false,true} bool_t;  
 typedef enum {WAIT_SOL,RX_ON,EOL_RECEIVED} RxStatus_t;
+//typedef enum {AT_IDLE, AT_IN_PROGRESS, AT_RX_COMPLETE} at_status_t;
 
-typedef enum{E5_OK,E5_ERROR} e5_error_t;
+typedef enum{
+            E5_OK,
+            E5_ERROR,
+            E5_TIME_OUT,
+            E5_BUSY} e5_error_t;
+
+typedef enum{TIME_OUT_STOPPED, TIME_OUT_RUNNING,TIME_OUT_EXPIRED} TimeOutStatus_t;
 
 
 
 #define E5_SOL      0x2B        // '+'
 #define E5_EOL      0x0A        // LF
 #define BUFFER_SIZE 65
+
+#define VAL_500MS   7812        // @Fcy=4MHz
+
+/*--------------------------------------------------------------------------*/
+/* Fonction  :  e5_error_t  E5_CheckConnection(void);                       */
+/* Description : Checks connection between MCU and LoRa-E5 module           */
+/*              using the "dummy" AT command                                */
+/* PE :                                                                     */
+/* ps :                                                                     */
+/* Mode d'emploi :                                                          */
+/*--------------------------------------------------------------------------*/
+e5_error_t  E5_CheckConnection(void);
 
 /*--------------------------------------------------------------------------*/
 /* Fonction  :                                                              */
@@ -32,7 +52,16 @@ typedef enum{E5_OK,E5_ERROR} e5_error_t;
 /* ps :                                                                     */
 /* Mode d'emploi :                                                          */
 /*--------------------------------------------------------------------------*/
-e5_error_t  E5_test_AT(void);
+e5_error_t  E5_Join(bool_t ForceJoin);
+
+/*--------------------------------------------------------------------------*/
+/* Fonction  :                                                              */
+/* Description :                                                            */
+/* PE :                                                                     */
+/* ps :                                                                     */
+/* Mode d'emploi :                                                          */
+/*--------------------------------------------------------------------------*/
+e5_error_t  E5_SendStrMsg(uint8_t *pMsg);
 
 /*--------------------------------------------------------------------------*/
 /* Fonction  :                                                              */
@@ -46,11 +75,14 @@ e5_error_t  E5_getVER(void);
 /*--------------------------------------------------------------------------*/
 /* Fonction  :                                                              */
 /* Description :                                                            */
-/* PE :                                                                     */
+/* PE : Time Out value, in x500ms Tic                                       */
 /* ps :                                                                     */
 /* Mode d'emploi :                                                          */
 /*--------------------------------------------------------------------------*/
-
+void                E5_startTimeOut(uint8_t Nb500ms);
+TimeOutStatus_t     E5_getTimeOutStatus(void);
+void                E5_ClearTimeOutStatus(void);
+void                E5_ReStartTimeOut(void);
 /*--------------------------------------------------------------------------*/
 /* Fonction  :                                                              */
 /* Description :                                                            */
@@ -71,12 +103,12 @@ void    E5_ll_send(uint8_t *pstr);
 
 /*--------------------------------------------------------------------------*/
 /* Fonction  :                                                              */
-/* Description :                                                            */
+/* Description : removes RC & LF cars from the rx frame                     */
 /* PE :                                                                     */
 /* ps :                                                                     */
 /* Mode d'emploi :                                                          */
 /*--------------------------------------------------------------------------*/
-
+void    E5_trim(uint8_t *pDest, uint8_t *pSource);
 
 /*--------------------------------------------------------------------------*/
 /* Fonction  :                                                              */
